d36cefa9a04ef63eaee60c520c4c70f34d23032c
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2025-06-10 10:10:48 +0200

    fix php principal

---
+++ config/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+// Variable a modifier pour passage en prod
+define ("ENV", "local");
+// define ("ENV", "prod");
+
+if (ENV === "local") {
+    define ('CSS_DIR', 'http://sliuba.local/dist/css/');
+    define ('JS_DIR', 'http://sliuba.local/dist/js/');
+    define ('IMG_DIR', 'http://sliuba.local/images/');
+    define ('BASE_DIR', 'http://sliuba.local/');
+} else {
+    define ('CSS_DIR', 'https://ilona.afecdax.ovh/dist/css/');
+    define ('JS_DIR', 'https://ilona.afecdax.ovh/dist/js/');
+    define ('IMG_DIR', 'https://ilona.afecdax.ovh/images/');
+    define ('BASE_DIR', 'https://ilona.afecdax.ovh/');
+}
--- php/config/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define ('CSS_DIR', 'http://sliuba.local/dist/css/');
-define ('JS_DIR', 'http://sliuba.local/dist/js/');
-define ('IMG_DIR', 'http://sliuba.local/images/');
-define ('BASE_DIR', 'http://sliuba.local/php/');
