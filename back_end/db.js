9c610f01af55f97f8e93b82f2a662fbc1ac9d41a
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2025-08-28 14:47:42 +0200

    sur branch tests

--- back_end/db.js
+++ back_end/db.js
@@ -7,10 +7,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
-    // port: process.env.DB_PORT,
-    // waitForConnection: true,
-    // connectionLimit: 10,
-    // queueLimit: 0
+
 });
 
 pool.connect((err) => {
---
+++ back_end/src/test.js
@@ -0,0 +1,3 @@
+const { SassCalculation } = require("sass");
+
+SassCalculation
